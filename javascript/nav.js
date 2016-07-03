1e4066ca1d965c8172617b460fdbd17398724c3f
littlemisspiss <github@littlemisspiss.xyz>
2016-07-02 22:48:29 -0600

    Update nav.js

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,7 +4,7 @@ angular.module('app', [])
   return {
     scope: true,
     restrict: 'E',
-    template: '<section class="navigation"><div class="navbar navbar-default navbar-fixed-top" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="/">LMP</a></div><div class="navbar-collapse collapse"><ul class="nav navbar-nav navbar-right"><li><a href="http://www.littlemisspiss.com/movie/" title="Think gay.">Movie</a></li></ul></div><!--/.nav-collapse --></div></div></section>',
+    template: '<section class="navigation"><div class="navbar navbar-default navbar-fixed-top" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="/">LMP</a></div><div class="navbar-collapse collapse"><ul class="nav navbar-nav navbar-right"><li><a href="http://www.littlemisspiss.xyz/movie/" title="Think gay.">Movie</a></li></ul></div><!--/.nav-collapse --></div></div></section>',
     link: function(scope, elem) {}
   };
 })
