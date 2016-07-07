cd0636a532b87a2293ed728d6989467a350baeaa
Your Name <you@example.com>
2016-07-06 19:40:31 -0600

    update

--- javascript/preloader.js
+++
@@ -1,4 +0,0 @@
-$(window).load(function() { // makes sure the whole site is loaded
-  $("#status").fadeOut(); // will first fade out the loading animation
-  $("#preloader").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-})
